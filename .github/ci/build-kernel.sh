af77bec6c4203db710fb3f9d54a64c686f02a6b6
Sanjay Chitroda <sanjayembeddedse@gmail.com>
2026-01-23 08:00:12 +0530

    build-kernel.sh: extend for stress profile
    add kconfig seed to have randconfig re-producible build
    add make flag specific to the stable/stress compilation

--- .github/ci/build-kernel.sh
+++ .github/ci/build-kernel.sh
@@ -13,6 +13,21 @@ echo "CONFIG=$CONFIG"
 echo "CROSS_COMPILE=$CROSS_COMPILE"
 echo "OUT=$KERNEL_OUT"
 
+# config seed to have re-producible build
+if [ "$CONFIG" = "randconfig" ]; then
+  export KCONFIG_SEED=${KCONFIG_SEED:-0x$(date +%Y%m%d)}
+  echo "SEED=$KCONFIG_SEED"
+fi
+
+case "${CI_PROFILE}" in
+  stable)
+    MAKE_FLAGS=""
+    ;;
+  stress)
+    MAKE_FLAGS="W=1 C=1"
+    ;;
+esac
+
 # Configure
 make O="$KERNEL_OUT" ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" "$CONFIG"
 
@@ -23,6 +38,7 @@ make -j$(nproc) \
   O="$KERNEL_OUT" \
   ARCH="$ARCH" \
   CROSS_COMPILE="$CROSS_COMPILE" \
+  $MAKE_FLAGS \
   2>&1 | tee "$LOG_FILE" | \
   grep -Ei --color=always "$FILTER_REGEX" \
   || true
